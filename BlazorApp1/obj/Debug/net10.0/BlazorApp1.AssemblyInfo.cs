c9fd2dcdb1930aed113937497d940f864e896ef6
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2026-03-20 21:01:07 +0100

    u

--- BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52aedd8cec7ee5c78248614d40fa5f8e786a93d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+460e9833b808abc2c179c94c32be99fd92171c5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
